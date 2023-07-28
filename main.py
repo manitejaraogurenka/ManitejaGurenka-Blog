80ac30a50fe64b925fa8c730fa2936554abd2f53
Maniteja Gurenka <140676670+ManitejaGurenka@users.noreply.github.com>
2023-07-28 06:38:29 +0530

    chaged import os main.py

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
 from sqlalchemy.ext.declarative import declarative_base
-import OS
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = OS.environ.get("SECRET_KEY")
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
@@ -51,7 +51,7 @@ def admin_only(f):
 
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = OS.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
